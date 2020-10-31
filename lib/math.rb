0af7c7b65cfbb32f40ed4fc3d06b3b542ac9ea67
Melissa F <melissa.faulner@gmail.com>
2020-10-31 15:01:51 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,17 +9,20 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-
+  result = num1 / num2
+  result
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  product
 end
 
 def modulo(num1, num2)
-
+  result = num1 % num2
+  result
 end
 
 def square_root(num)
-
+  
 end
